f722e008ba14175a6595347c2bb19bd69399a810
Laura Ojala <laojala@gmail.com>
2019-12-14 15:29:41 +0200

    Day 8, part 2 using JavaScript

--- Day8_Space_Image_Format/part1.js
+++ Day8_Space_Image_Format/part1.js
@@ -6,13 +6,10 @@ const width = 25
 const height = 6
 
 const image = data.map(item => Number(item))
-
 const layeredData = []
-
 while (image.length) {
     layeredData.push(image.splice(0, width*height))
 }
-
 
 const part1_checksum = () => {
     let numberOfZeroDigits = layeredData[0].length
@@ -30,4 +27,47 @@ const part1_checksum = () => {
     return fewestZeroDigits[0].filter(digit => digit === 1).length * fewestZeroDigits[0].filter(digit => digit === 2).length
 } //part1_checksum
 
-console.log(`Result is 1584: ${part1_checksum() === 1584}`)
+console.log(`Part 1 result is 1584: ${part1_checksum() === 1584}`)
+
+
+//part2
+const part2_decode = () => {
+
+    const getColor = (arrayOfArrays, index) => 
+        
+    arrayOfArrays.find(array => {
+        if (array[index] === 0 || array[index] === 1)
+            return array
+    })
+
+    //creating transparent Array
+    const transparent = new Array(width*height)
+    transparent.fill(2)
+
+    //filling just created transparent array
+    const finalImage = transparent.map((item, index) => {
+    let array = getColor(layeredData, index)
+    if (array)
+        return array[index]
+    else 
+        return item
+    })
+
+    const toBePrinted = []
+
+    while (finalImage.length) 
+        toBePrinted.push(finalImage.splice(0, 25))
+    
+    toBePrinted.forEach(row => {
+        temp = []
+        row.forEach(char => {
+            if (char === 0)
+                temp.push(" ")
+            else
+                temp.push("█")
+        })
+        console.log(temp.join(''))
+    })
+}
+
+part2_decode()
